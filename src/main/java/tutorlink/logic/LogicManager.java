6bfaf3c7c14d9fbd0c5510d5c9875dc7060b8613
RCPilot1604 <ethan.chua1604@gmail.com>
2024-10-27 16:46:13 +0800

    Add :LogicManager

---
+++ src/main/java/tutorlink/logic/LogicManager.java
@@ -0,0 +1,21 @@
+package tutorlink.logic;
+
+import java.util.HashMap;
+import tutorlink.logic.command.Command;
+import tutorlink.logic.parser.Parser;
+import tutorlink.logic.result.CommandResult;
+import tutorlink.model.AppState;
+
+public class LogicManager {
+    private static final Parser parser = new Parser();
+
+    public CommandResult execute(AppState appState, String line) {
+        //@@author yeekian
+        Command currentCommand = parser.getCommand(line);
+        HashMap<String, String> arguments = parser.getArguments(currentCommand.getArgumentPrefixes(), line);
+        CommandResult res = currentCommand.execute(appState, arguments);
+        //@@author
+        return res;
+    }
+
+}
