1d7863ba61b82e9f4059d9d53d165d60bce8d9cb
RCPilot1604 <ethan.chua1604@gmail.com>
2024-10-27 14:12:17 +0800

    Remove static declaration of AppState for JUnit tests

--- src/main/java/tutorlink/model/AppState.java
+++ src/main/java/tutorlink/model/AppState.java
@@ -5,9 +5,9 @@ import tutorlink.model.lists.GradeList;
 import tutorlink.model.lists.StudentList;
 
 public class AppState {
-    public static StudentList students = new StudentList();
-    public static GradeList grades = new GradeList();
-    public static ComponentList components = new ComponentList();
+    public StudentList students = new StudentList();
+    public GradeList grades = new GradeList();
+    public ComponentList components = new ComponentList();
 
     public AppState() {
     }
